69c3c1a378ca8c62f6dedf8a508ebc778f7cd746
Bruno Faganello <bruno.faganello.neto@gmail.com>
2017-08-16 10:04:52 -0300

    Arquivo de configuracoes

---
+++ US/index.php
@@ -0,0 +1,178 @@
+
+
+
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Creative Apps</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/flexslider.css">
+<link rel="stylesheet" href="css/jquery.fancybox.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="css/font-icon.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<!-- header section -->
+<section class="banner" role="banner">
+  <!--header navigation -->
+  <header id="header">
+    <div class="header-content clearfix"> <a class="logo" href="#top"><img src="images/logo.png" alt=""></a>
+      <nav class="navigation" role="navigation">
+        <ul class="primary-nav">
+          <li><a href="#intro">O que é ser Creative?</a></li>
+          <li><a href="#services">Por que ser Creative?</a></li>
+          <li><a href="#contact">Be Creative</a></li>
+        </ul>
+      </nav>
+      <a href="#" class="nav-toggle">Menu<span></span></a> </div>
+  </header>
+  <!--header navigation -->
+  <!-- banner text -->
+  <div class="container">
+    <div class="col-md-10 col-md-offset-1">
+      <div class="banner-text text-center">
+        <h1><b>C</b>REATIVE</h1>
+        <p>Agência Digital</p>
+        <nav role="navigation"> <a href="#intro" class="btn btn-large">Descubra</a> </div></nav>
+      </div>
+      <!-- banner text -->
+    </div>
+  </div>
+</section>
+<!-- header section -->
+<!-- about section -->
+<section id="intro" class="section intro no-padding">
+  <div class="container-fluid">
+    <div class="row no-gutter">
+      <div class="flexslider">
+        <ul class="slides">
+          <li>
+            <div class="col-md-6">
+              <div class="avatar"> <img src="images/intro-img1.jpg" alt="" class="img-responsive"> </div>
+            </div>
+            <div class="col-md-6">
+              <blockquote>
+                <h1>O que é ser <b>C</b>reative?</h1>
+                <h3>A mudança é certa</h3>
+                <p>O mundo está em uma constante transformação, a forma como pessoas interagem.
+                  Dessa forma pensamos em soluções que coloque seus cliente em primeiro lugar em qualquer lugar. </p>
+                  <p>Nós da Creative, entendemos que a única certeza no mundo é a mudança. Por isso impulsionamos empresas rumo à transformação de seus produtos e serviços na era digital.</p>
+              </blockquote>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- about section -->
+
+<!-- services section -->
+<section id="services" class="services service-section">
+  <div class="col-md-8 col-md-offset-2 text-center">
+    <h1>Por que ser Creative?</h1>
+    <br>
+    <br>
+  </div>
+  <div class="container">
+
+    <div class="row">
+      <div class="col-md-4 col-sm-6 services"> <span class="icon icon-briefcase"></span>
+        <div class="services-content">
+          <h4>Criação de Identidade</h4>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 services"> <span class="icon icon-tools"></span>
+        <div class="services-content">
+          <h4>Design e Desenvolvimento</h4>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 services"> <span class="icon icon-megaphone"></span>
+        <div class="services-content">
+          <h4>Marketing Digital</h4>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- services section -->
+<!-- Get a quote section -->
+<section id="contact" class="section quote">
+  <div class="container">
+    <div class="col-md-8 col-md-offset-2 text-center">
+      <h3>Looking for a new brand? Let's work together!</h3>
+      <a href="#" class="btn btn-large">Get Quotation</a> </div>
+  </div>
+</section>
+<!-- Get a quote section -->
+<!-- Footer section -->
+<footer class="footer">
+  <div class="footer-top section">
+    <div class="container">
+      <div class="row">
+        <div class="footer-col col-md-6">
+          <h5>Estamos pelo Mundo</h5>
+          <p>São Paulo/SP.<br>
+            comunicacao@creativeapps.com.br</p>
+          <p>Copyright © 2017 Creative Apps. All Rights Reserved.</p>
+        </div>
+        <div class="footer-col col-md-3">
+          <h5>Services We Offer</h5>
+          <p>
+          <ul>
+            <li><a href="#">Marketing Digital</a></li>
+            <li><a href="#">Websites</a></li>
+            <li><a href="#">Videography</a></li>
+            <li><a href="#">Social Media</a></li>
+            <li><a href="#">User Experience</a></li>
+          </ul>
+          </p>
+        </div>
+        <div class="footer-col col-md-3">
+          <h5>Share with Love</h5>
+          <ul class="footer-share">
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- footer top -->
+
+</footer>
+<!-- Footer section -->
+<!-- JS FILES -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.flexslider-min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/retina.min.js"></script>
+<script src="js/modernizr.js"></script>
+<script src="js/main.js"></script>
+<!-- CHAT BOT-->
+<link href="https://api.motion.ai/sdk/webchat.css" rel="stylesheet" type="text/css">
+<script src="https://api.motion.ai/sdk/webchat.js"></script>
+<script>
+   motionAI_Init('77004?color=5C45A5&sendBtn=Enviar&inputBox=Digitando.....&token=28a820313bd0801b915f79b474ace94f',true,400,470,'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Circle-icons-chat.svg/1024px-Circle-icons-chat.svg.png');
+   /* You may also invoke motionAI_Open() to manually open the modal. */
+</script>
+</body>
+</html>
